fe151beb6356ac0764c23404e15bcb506b206d62
Ali Murad <alimurad00@hotmail.com>
2025-02-24 18:11:36 +0100

    Removed backend from main:app

--- backend/Modules/config.py
+++ backend/Modules/config.py
@@ -6,7 +6,6 @@ config = ConfigParser()
 
 load_dotenv()
 
-
 # Hugging Face API Key
 HUGGINGFACE_KEY = os.getenv("HUGGINGFACE_KEY")
 
@@ -16,8 +15,6 @@ OPENAI_KEY = os.getenv("OPENAI_KEY")
 # Hugging Face Model Names
 DEEPSEEK_MODEL = "deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
 FALCON_MODEL = "tiiuae/falcon-7b-instruct"
-LLAMA_MODEL_70B = "meta-llama/Llama-2-70b-chat-hf"
-MISTRAL_MODEL = "mistralai/Mistral-Large-Instruct-2407"
 
 # Vector Database Configuration
 VECTOR_DB_PATH = os.getenv("VECTOR_DB_PATH", "vector_store")
@@ -26,4 +23,7 @@ VECTOR_DB_PATH = os.getenv("VECTOR_DB_PATH", "vector_store")
 MINI_LM_EMBED = 'sentence-transformers/all-MiniLM-L6-v2'
 OPENAI_EMBED = 'text-embedding-3-large'
 
+
+if __name__ == "__main__":
+    print("Hugging Face API Key: ", HUGGINGFACE_KEY)
 
--- backend/main.py
+++ backend/main.py
@@ -21,4 +21,4 @@ def root():
     return {"message": "Welcome to the Chatbot API!"}
 
 if __name__ == '__main__':
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
